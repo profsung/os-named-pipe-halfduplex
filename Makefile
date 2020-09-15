b56add55b7242b30c10940f6e7a2404369d701fb
Hong Sung <hs@cs.uco.edu>
2020-09-15 14:09:35 -0500

    init

---
+++ Makefile
@@ -0,0 +1,5 @@
+p1: prog1.cpp
+	g++ -g prog1.cpp -o p1.out
+
+p2: prog2.cpp
+	g++ -g prog2.cpp -o p2.out
---
+++ prog2.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	const char *myfifo = ".myfifo";
+
+	// create the named pipe (FIFO)
+	mkfifo(myfifo, 0666);
+
+	const int MAX = 1024;
+	char rd_data[MAX], wr_data[MAX];
+
+	while (true) {
+		int fd = open(myfifo, O_RDONLY);
+		read(fd, rd_data, sizeof(rd_data));
+		printf("received: %s\n", rd_data);
+		close(fd);
+		if (strcmp(rd_data, "quit") == 0) {
+			break;
+		}
+
+		fd = open(myfifo, O_WRONLY);
+		printf("Enter a message: ");
+		fgets(wr_data, MAX, stdin);
+		wr_data[strlen(wr_data) - 1] = '\0';
+		write(fd, wr_data, strlen(wr_data) + 1);
+		close(fd);
+	}
+}
